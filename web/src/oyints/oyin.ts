213564b4795b5d9fa3eabd3d8637f95f2d54cfc2
Muhammad <muhammadjonrasuljonov2002@gmail.com>
2023-12-13 08:30:10 +0500

    hdsjhde

--- web/src/oyints/oyin.ts
+++ web/src/oyints/oyin.ts
@@ -3,7 +3,6 @@ import { zz } from "./reflesh";
 const player2: HTMLParagraphElement = document.querySelector(".name1")!;
 const player1: HTMLParagraphElement = document.querySelector(".name")!;
 const paly: HTMLButtonElement = document.querySelector(".paly")!;
-
 const conb: HTMLDivElement = document.querySelector(".conb")!;
 const url = "http://localhost:4000/api/tic-tac-toe/move/";
 
@@ -14,23 +13,25 @@ export function gameCell() {
 		conb.appendChild(p);
 	}
 }
+
 const reflesh: HTMLButtonElement = document.querySelector(".reflesh")!;
+
+let currentPlayer: string = "X";
 
 export function name(id: any) {
 	const cells: NodeListOf<HTMLParagraphElement> = document.querySelectorAll(".p")!;
-	let currentPlayer: string = "X";
-
 	cells.forEach((cell, i) => {
 		cell.addEventListener("click", () => {
-			if (cell.innerText === "") {
-				cell.innerText = currentPlayer;
-				currentPlayer = currentPlayer === "X" ? "O" : "X";
 
-				paly.innerText = currentPlayer;
+				if (cell.innerText === "" ) {
+					cell.innerText = currentPlayer;
+					currentPlayer = currentPlayer === "X" ? "O" : "X";
+
 			}
 			games(id, currentPlayer, i);
 			reflesh.addEventListener("click", () => {
 				zz(id);
+				location.reload();
 			});
 		});
 	});
@@ -51,16 +52,33 @@ function games(id: any, currentPlayer: any, i: any) {
 		.then((response) => response.json())
 		.then((data) => {
 			console.log(data);
-
-			if (data.player1 !== undefined) {
-				player1.innerText = data.player1;
-			}
-
-			if (data.player2 !== undefined) {
-				player2.innerText = data.player2;
-			}
+			updateUI(data);
+			XO(data);
 		})
 		.catch((error) => {
 			console.error("Error:", error);
 		});
+}
+
+function updateUI(data: any) {
+	player2.innerText = data.data.player2;
+	player1.innerText = data.data.player1;
+
+	if (currentPlayer === "X") {
+		paly.innerText = `${data.data.player1} = ${currentPlayer}`;
+	} else {
+		paly.innerText = `${data.data.player2} = ${currentPlayer}`;
+	}
+}
+
+function XO(data: any) {
+	const cells: NodeListOf<HTMLParagraphElement> = document.querySelectorAll(".p")!;
+
+	for (let i = 0; i < 9; i++) {
+		if (data && data.board && Array.isArray(data.board) && data.board.length === 9) {
+			for (let i = 0; i < 9; i++) {
+				cells[i].innerText = data.board[i];
+			}
+		}
+	}
 }
